bf9efe31a29f32745e3dcc5e43255795dc15ee2f
Sandi Barr <sandi.k.barr@gmail.com>
2024-09-24 17:41:01 +0000

    add vertex ai

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+// server.ts
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
@@ -5,53 +6,80 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 
+import { VertexAI } from '@google-cloud/vertexai';
+import { GoogleAuth } from 'google-auth-library';
+
+
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
-  const server = express();
-  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
+    const server = express();
+    const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+    const browserDistFolder = resolve(serverDistFolder, '../browser');
+    const indexHtml = join(serverDistFolder, 'index.server.html');
+    const commonEngine = new CommonEngine();
 
-  const commonEngine = new CommonEngine();
+    const auth = new GoogleAuth();
 
-  server.set('view engine', 'html');
-  server.set('views', browserDistFolder);
+    server.set('view engine', 'html');
+    server.set('views', browserDistFolder);
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html',
-  }));
+    // Example Express Rest API endpoints
+    server.get('/api/facts', async (req, res) => {
+        const project = await auth.getProjectId();
 
-  // All regular routes use the Angular engine
-  server.get('**', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
+        const vertex = new VertexAI({ project: project });
+        const generativeModel = vertex.getGenerativeModel({
+            model: 'gemini-1.5-flash',
+            generationConfig: { responseMimeType: 'application/json' }
+        });
 
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
-  });
+        const animal = req.query['animal'] || 'dog';
+        const prompt = `Give me 10 fun facts about ${animal}. 
+                        Return as json as an array in the format ['fact 1', 'fact 2']
+                        Remove backticks and other markdown formatting.`;
 
-  return server;
+        const resp = await generativeModel.generateContent(prompt);
+        let factArray = '';
+
+        if (resp.response.candidates) {
+            factArray = JSON.parse(resp.response.candidates[0].content.parts[0].text || '');
+        }
+        res.send(factArray);
+    });
+
+    // Serve static files from /browser
+    server.get('**', express.static(browserDistFolder, {
+        maxAge: '1y',
+        index: 'index.html',
+    }));
+
+    // All regular routes use the Angular engine
+    server.get('**', (req, res, next) => {
+        const { protocol, originalUrl, baseUrl, headers } = req;
+
+        commonEngine
+            .render({
+                bootstrap,
+                documentFilePath: indexHtml,
+                url: `${protocol}://${headers.host}${originalUrl}`,
+                publicPath: browserDistFolder,
+                providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+            })
+            .then((html) => res.send(html))
+            .catch((err) => next(err));
+    });
+
+    return server;
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+    const port = process.env['PORT'] || 4000;
 
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+    // Start up the Node server
+    const server = app();
+    server.listen(port, () => {
+        console.log(`Node Express server listening on http://localhost:${port}`);
+    });
 }
 
 run();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, FormsModule],
     template: `
-        <h1>Hello world!</h1>
-
-        <router-outlet />
+        <section>
+            <input
+                type="text"
+                placeholder="dog"
+                [(ngModel)]="animal"
+                class="text-black border-2 p-2 m-2 rounded"
+            />
+            <button
+                (click)="getNewFunFacts()"
+            >
+                Get New Fun Facts
+            </button>
+            <ol>
+                @for(fact of facts(); track fact) {
+                    <li>{{fact}}</li>  
+                } @empty {
+                    <li>No facts are available</li>
+                }
+            </ol>
+        </section>
     `,
-    styles: [],
+    styles: '',
 })
-export class AppComponent { }
+export class AppComponent {
+    animal = '';
+    facts = signal<string[]>([]);
+
+    getNewFunFacts() {
+        fetch(`/api/facts?animal=${this.animal}`).then(response => response.json()).then(facts => {
+            this.facts.set(facts);
+        });
+    }
+}
